d19ca22b74399ccf248f92a4fe2bbb65610ad0d9
Spazi <talishmarprkle+g@gmail.com>
2022-11-17 08:51:50 +0200

    some movement

--- Assets/Scripts/MonsterMovement.cs
+++ Assets/Scripts/MonsterMovement.cs
@@ -7,21 +7,29 @@ public class MonsterMovement : MonoBehaviour
 	[SerializeField] Vector3 MovementBoundaryMax;
 	[SerializeField] Vector3 MovementBoundaryMin;
 	[SerializeField] float forwardSpeed;
+	[SerializeField] float turnSpeed;
+	Transform player;
 	Vector3 ForwardDirection;
 	void Start()
 	{
 		ForwardDirection = Vector3.forward;
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 	}
 
 	void Update()
 	{
-		transform.Translate(ForwardDirection * forwardSpeed * Time.deltaTime);
+		transform.Translate(ForwardDirection * forwardSpeed * Time.deltaTime); //Move forward
+
+		//Check if passed any boundary
 		if (transform.position.x > MovementBoundaryMax.x 
 		|| transform.position.x < MovementBoundaryMin.x 
 		|| transform.position.z > MovementBoundaryMax.z
 		|| transform.position.z < MovementBoundaryMin.z)
-		{
-			Debug.Log("Monster out of bounds");
+		{	//Rotate towards player
+			Vector3 playerFlatPosition = new Vector3(player.position.x, transform.position.y, player.position.y); //Use own Y position instead as target's, to not move up or down
+			Vector3 relativePos = playerFlatPosition - transform.position; //Create a vector pointing to target from own position
+			Quaternion desiredRotation = Quaternion.LookRotation(relativePos, Vector3.up); //Convert said vector to quaternion
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRotation, turnSpeed * Time.deltaTime); //Rotate
 		}
 	}
 	void OnDrawGizmosSelected()
