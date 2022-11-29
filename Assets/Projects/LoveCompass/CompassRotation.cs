c20c45ad860b993d268356fc785fadb11bd5d403
Spazi <talishmarprkle+g@gmail.com>
2022-11-29 18:53:07 +0200

    Compass works, attached to player prefab (drag in target reference)

---
+++ Assets/Projects/LoveCompass/CompassRotation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassRotation : MonoBehaviour
+{
+	
+
+	[SerializeField] Transform targetTransform;
+	Vector3 target;
+
+	void Update()
+	{
+			target = transform.position + (targetTransform.position - transform.position); // Vector from self to player
+			target = Vector3.ProjectOnPlane(target - transform.position, transform.up) + transform.position; //Flatten vector against own up direction
+			transform.LookAt(target, transform.parent.transform.up); //Convert vector to transform rotation quaternion
+			Debug.DrawLine(transform.position, target, Color.red, .1f);
+	}
+
+}
