1e2f623b8fdaa04b52e5c8039a64c6595a371cda
Vallu368 <vsavolainen22@gmail.com>
2022-12-14 13:02:43 +0200

    spotlights turns off too now

--- Assets/Scripts/LightScript.cs
+++ Assets/Scripts/LightScript.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class LightScript : MonoBehaviour
 {
     public Light lampLight;
     public bool lightOn;
+    public GameObject spotLight;
     void Start()
     {
         lampLight = GameObject.Find("LampLight").GetComponent<Light>();
+        spotLight = GameObject.Find("Spot Light");
+
     }
 
     // Update is called once per frame
@@ -20,11 +24,13 @@ public class LightScript : MonoBehaviour
             {
                 lampLight.enabled = false;
                 lightOn = false;
+                spotLight.SetActive(false);
             }
             else if(!lightOn)
             {
                 lampLight.enabled = true;
                 lightOn = true;
+                spotLight.SetActive(true);
             }
         }
     }
